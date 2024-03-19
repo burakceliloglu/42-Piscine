754a77e04db5da2a6c718b436161f97c276e6a7b
Burak Celiloğlu <bkceliloglu@gmail.com>
2024-03-19 23:13:53 +0300

    ex04 added.

---
+++ c11/ex03/ft_count_if.c
@@ -0,0 +1,42 @@
+/* if one of the char array doesnt
+contain 'a', then count++
+int	function(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == 'a')
+			return (0);
+		i++;
+	}
+	return (1);
+} 
+*/
+
+int	ft_count_if(char **tab, int length, int(*f)(char*))
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (i < length)
+	{
+		if (f(tab[i]) != 0)
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+/*
+#include <stdio.h>
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	int result = ft_count_if(argv, argc, &function);
+	printf("%d", result);
+}
+*/
